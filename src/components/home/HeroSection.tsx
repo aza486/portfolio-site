2ed02059712199e43dcf86be7b4d5dd4b77fd06e
aza486 <d.podjapolski@web.de>
2026-06-26 13:39:50 +0200

    added bg and font

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,15 +1,17 @@
+import "./HeroSection.css";
+
 function HeroSection() {
   return (
-    <div className="hero-section">
+    <div className="hero-section" >
       <p className="hero-name">Daniel Podjapolski</p>
 
       <h1 className="hero-title">
-        Hallo, ich bin
-        <br />
-        Designer&Developer
+        Hallo, ich bin Designer&Developer
       </h1>
     </div>
   );
 }
 
 export default HeroSection;
+
+
