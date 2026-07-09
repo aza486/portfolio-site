052d1e3993dcde4a906514991cfdfe68c6191e69
aza486 <d.podjapolski@web.de>
2026-07-09 14:15:14 +0200

    TechStack-swiper: Add a swiper for the tech stack section to allow horizontal scrolling of tech icons.

--- src/components/home/profile/TechStack.tsx
+++ src/components/home/profile/TechStack.tsx
@@ -1,25 +1,50 @@
-const techStack = [
-  { label: "A", name: "Angular", tone: "angular" },
-  { label: "R", name: "React", tone: "react" },
-  { label: "n8n", name: "n8n", tone: "n8n" },
-  { label: "G", name: "Go", tone: "go" },
-  { label: "JS", name: "JavaScript", tone: "javascript" },
-  { label: "5", name: "HTML", tone: "html" },
-  { label: "3", name: "CSS", tone: "css" },
-];
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+
+import "./ProfileSection.css";
+import { techStack } from "../../../data/techStack";
 
 function TechStack() {
   return (
     <section className="tech-stack">
       <h2>Techstack</h2>
 
-      <div className="tech-stack-list" aria-label="Techstack">
-        {techStack.map((tech) => (
-          <span className={`tech-badge tech-badge-${tech.tone}`} key={tech.name}>
-            {tech.label}
-          </span>
-        ))}
-      </div>
+        <Swiper
+          modules={[Autoplay]}
+          className="tech-swiper"
+          slidesPerView="auto"
+          spaceBetween={36}
+          loop
+          speed={2000}
+          grabCursor={true}
+          allowTouchMove={true}
+          simulateTouch={true}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: false,
+            waitForTransition: true,
+          }}
+        >
+          {techStack.map((tech) => (
+            <SwiperSlide
+              key={tech.name}
+              className="tech-slide"
+            >
+              <img
+                src={tech.icon}
+                alt={tech.name}
+                className="tech-icon"
+                style={{
+                  transform: `scale(${tech.scale})`,
+                }}
+              />
+            </SwiperSlide>
+          ))}
+
+        </Swiper>
     </section>
   );
 }
---
+++ src/data/techStack.ts
@@ -0,0 +1,111 @@
+import adobe from "../assets/icons/adobexd.svg";
+import css from "../assets/icons/css.svg";
+import docker from "../assets/icons/docker.svg";
+import figma from "../assets/icons/figma.svg";
+import git from "../assets/icons/git.svg";
+import go from "../assets/icons/go.svg";
+import html from "../assets/icons/html.svg";
+import illustrator from "../assets/icons/illustrator.svg";
+import indesign from "../assets/icons/indesign.svg";
+import javascript from "../assets/icons/javascript.svg";
+import n8n from "../assets/icons/n8n.svg";
+import nodejs from "../assets/icons/nodejs.svg";
+import photoshop from "../assets/icons/photoshop.svg";
+import postgres from "../assets/icons/postgres.svg";
+import premiere from "../assets/icons/premierepro.svg";
+import react from "../assets/icons/react.svg";
+import shopware from "../assets/icons/shopware.svg";
+import typescript from "../assets/icons/typescript.svg";
+
+export const techStack = [
+  {
+    name: "React",
+    icon: react,
+    scale: 1,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+    scale: 1,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+    scale: 1,
+  },
+  {
+    name: "Node.js",
+    icon: nodejs,
+    scale: 1,
+  },
+  {
+    name: "Go",
+    icon: go,
+    scale: 1,
+  },
+  {
+    name: "Docker",
+    icon: docker,
+    scale: 1,
+  },
+  {
+    name: "PostgreSQL",
+    icon: postgres,
+    scale: 1,
+  },
+  {
+    name: "Git",
+    icon: git,
+    scale: 1,
+  },
+  {
+    name: "n8n",
+    icon: n8n,
+    scale: 1.25,
+  },
+  {
+    name: "Shopware",
+    icon: shopware,
+    scale: 1,
+  },
+  {
+    name: "Figma",
+    icon: figma,
+    scale: 1,
+  },
+  {
+    name: "Adobe XD",
+    icon: adobe,
+    scale: 1,
+  },
+  {
+    name: "Illustrator",
+    icon: illustrator,
+    scale: 1,
+  },
+  {
+    name: "Photoshop",
+    icon: photoshop,
+    scale: 1,
+  },
+  {
+    name: "InDesign",
+    icon: indesign,
+    scale: 1,
+  },
+  {
+    name: "Premiere Pro",
+    icon: premiere,
+    scale: 1,
+  },
+  {
+    name: "HTML5",
+    icon: html,
+    scale: 1,
+  },
+  {
+    name: "CSS3",
+    icon: css,
+    scale: 1,
+  },
+];
